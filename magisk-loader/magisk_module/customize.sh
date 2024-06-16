620ea71c9d2f71ef12d2adb36fc61515880af494
Li Hua <lihua@email.com>
2024-06-17 07:36:41 +0800

    Drop riru support
    Signed-off-by: Li Hua <lihua@email.com>

--- magisk-loader/magisk_module/customize.sh
+++ magisk-loader/magisk_module/customize.sh
@@ -58,14 +58,6 @@ check_android_version
 check_magisk_version
 check_incompatible_module
 
-if [ "$FLAVOR" == "riru" ]; then
-  # Extract riru.sh
-  extract "$ZIPFILE" 'riru.sh' "$TMPDIR"
-  . "$TMPDIR/riru.sh"
-  # Functions from riru.sh
-  check_riru_version
-fi
-
 enforce_install_from_magisk_app
 
 # Check architecture
@@ -110,44 +102,6 @@ if [ "$FLAVOR" == "zygisk" ]; then
       mv "$MODPATH/zygisk/liblspd.so" "$MODPATH/zygisk/x86_64.so"
     fi
   fi
-elif [ "$FLAVOR" == "riru" ]; then
-  mkdir "$MODPATH/riru"
-  mkdir "$MODPATH/riru/lib"
-  mkdir "$MODPATH/riru/lib64"
-  if [ "$ARCH" = "arm" ] || [ "$ARCH" = "arm64" ]; then
-    ui_print "- Extracting arm libraries"
-    extract "$ZIPFILE" "lib/armeabi-v7a/lib$RIRU_MODULE_LIB_NAME.so" "$MODPATH/riru/lib" true
-
-    if [ "$IS64BIT" = true ]; then
-      ui_print "- Extracting arm64 libraries"
-      extract "$ZIPFILE" "lib/arm64-v8a/lib$RIRU_MODULE_LIB_NAME.so" "$MODPATH/riru/lib64" true
-    fi
-  fi
-
-  if [ "$ARCH" = "x86" ] || [ "$ARCH" = "x64" ]; then
-    ui_print "- Extracting x86 libraries"
-    extract "$ZIPFILE" "lib/x86/lib$RIRU_MODULE_LIB_NAME.so" "$MODPATH/riru/lib" true
-
-    if [ "$IS64BIT" = true ]; then
-      ui_print "- Extracting x64 libraries"
-      extract "$ZIPFILE" "lib/x86_64/lib$RIRU_MODULE_LIB_NAME.so" "$MODPATH/riru/lib64" true
-    fi
-  fi
-
-  if [ "$RIRU_MODULE_DEBUG" = true ]; then
-    mv "$MODPATH/riru" "$MODPATH/system"
-    mv "$MODPATH/system/lib/lib$RIRU_MODULE_LIB_NAME.so" "$MODPATH/system/lib/libriru_$RIRU_MODULE_LIB_NAME.so"
-    mv "$MODPATH/system/lib64/lib$RIRU_MODULE_LIB_NAME.so" "$MODPATH/system/lib64/libriru_$RIRU_MODULE_LIB_NAME.so"
-    if [ "$RIRU_API" -ge 26 ]; then
-      mkdir -p "$MODPATH/riru/lib"
-      mkdir -p "$MODPATH/riru/lib64"
-      touch "$MODPATH/riru/lib/libriru_$RIRU_MODULE_LIB_NAME"
-      touch "$MODPATH/riru/lib64/libriru_$RIRU_MODULE_LIB_NAME"
-    else
-      mkdir -p "/data/adb/riru/modules/$RIRU_MODULE_LIB_NAME"
-    fi
-  fi
-fi
 
 if [ "$API" -ge 29 ]; then
   ui_print "- Extracting dex2oat binaries"
