7b96cffe2b0cc67fd80eb8e0b230226a220ef9d4
Li Hua <lihua@email.com>
2024-07-14 16:46:49 +0800

    Update
    Signed-off-by: Li Hua <lihua@email.com>

--- magisk-loader/magisk_module/customize.sh
+++ magisk-loader/magisk_module/customize.sh
@@ -61,7 +61,7 @@ check_incompatible_module
 enforce_install_from_magisk_app
 
 # Check architecture
-if [ "$ARCH" != "arm" ] && [ "$ARCH" != "arm64" ] && [ "$ARCH" != "x86" ] && [ "$ARCH" != "x64" ]; then
+if [ "$ARCH" != "arm" ] && [ "$ARCH" != "arm64" ] && [ "$ARCH" != "x86" ] && [ "$ARCH" != "x64" ] && ["$ARCH" != "riscv64"]; then
   abort "! Unsupported platform: $ARCH"
 else
   ui_print "- Device platform: $ARCH"
